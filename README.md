2751c0da7973273fe8ac5dbf76875cff055e4f91
jun3pr <jun3pr@hotmail.com>
2019-10-25 20:56:30 -0500

    changed to simple trivia & remade all of the syntax

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# TriviaGame
+<h1>Sports Trivia Game</h1>
+
+<h6>If you know your sports this is the game for you!</h6>
+<p>This game has 8 questions you have to answer in 60 seconds(one minute) so you better be ready. AT the end of the game it won't reveal the incorrect answers, if you a couple wrong pick yourself up and try again.<br>
+This game was made with HTML, CSS, Javascript & jQuery!</p>
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,185 +1,250 @@
-$(document).ready(function(){
-  
-    // event listeners
-    $("#remaining-time").hide();
-    $("#start").on('click', trivia.startGame);
-    $(document).on('click' , '.option', trivia.guessChecker);
-    
-  })
-  
-  var trivia = {
-    // trivia properties
-    correct: 0,
-    incorrect: 0,
-    unanswered: 0,
-    currentSet: 0,
-    timer: 20,
-    timerOn: false,
-    timerId : '',
-    // questions options and answers data
-    questions: {
-      q1: "Which team is as well-known for their comic antics as for their on-court skills?"
-      q2: 
-      q3: 
-      q4: 
-      q5:
-      q6: 
-      q7:
-      q8: 
-    },
-    options: {
-      q1: [New Zealand Rugby Team, Miami Dolphins, Harlem Globetrotters, Flint Tropics],
-      q2: [],
-      q3: [],
-      q4: [],
-      q5: [],
-      q6: [],
-      q7: []
-      q8: []
-    },
-    answers: {
-      q1: 
-      q2: 
-      q3: 
-      q4: 
-      q5: 
-      q6: 
-      q7: 
-      q8:
-    },
+let number = 60;
+
+let intervalId;
+
+let correctAnswers = 0;
+let incorrectAnswers = 0;
+let unanswered = 0;
+
+// Questions array
+    //Question1
+var questions = [{
+    question: "Which team is as well-known for their comic antics as for their athletic skills?",
+    answerList: ["New Zealand Rugby","Harlem Globetrotters", "FlintTropics","New York Mets"], 
+    answer: 1
+},{
+    //Question2
+    question: "Which is the only Major League Baseball team to never make it to a World Series?",
+    answerList: ["Seattle Mariners", "Miami Marlins", "Kansas City Royals", "Toronto Blue Jays"],
+    answer: 0
+},{
+    //Question3
+    question: "Indy 500 winners celebrate winning by drinking what?",
+    answerList: ["Beer", "Tomato Juice", "Water", "Milk"],
+    answer: 3
+},{
+    //Question4
+    question: "In the game of darts what is the highest possible score using three darts?",
+    answerList: ["220", "180", "140", "100"],
+    answer: 1
+},{
+    //Question5
+    question: "Which country won the first World Cup?",
+    answerList: ["Germany", "Brazil", "Uruguay", "USA"],
+    answer: 2
+},{
+    //Question6
+    question: "Who is the only athlete to appear in a Football game and a Baseball game on the same day?",
+    answerList: ["Bo Jackson", "Brian Jordan", "Deion Sanders", "Dave WInfield"],
+    answer: 0
+},{
+    //Question7
+    question: "Which NBA team has produced the most MVP players all time?",
+    answerList: ["Boston Celtics", "LA Lakers", "Chicago Bulls", "Houston Rockets"],
+    answer: 0
+},{
+    //Question
+    question: "Who has won the most Major Championships in golf history?",
+    answerList: ["Tiger Woods", "Walter Hagen", "Arnold Palmer", "Jack Nicklaus"],
+    answer: 3
+}];
+
+
+$("#start").on("click", function() {
+
+    // Hide Start 
+    $(this).hide();
+
+    // Display initial time countdown
+    $("#time").html("<h2>Time Remaining: 60 Seconds</h2>" + "<br>");
+
+    // Start timer 
+    run();
    
-   // trivia methods
-  
-  startGame: function(){
-    // restarting game results
-    trivia.currentSet = 0;
-    trivia.correct = 0;
-    trivia.incorrect = 0;
-    trivia.unanswered = 0;
-    clearInterval(trivia.timerId);
-    
-    // show game section
-    $('#game').show();
-    
-    //  empty last results
-    $('#results').html('');
-    
-    // show timer
-    $('#timer').text(trivia.timer);
-    
-    // remove start button
-    $('#start').hide();
+    // Display questions --- I still want to turn this into a reusable piece so that i don't have to repeat this section for each question
+    // Question 1
+    $("#question1").html("<h3>" + questions[0].question + "</h3>");
+    $("#answer1").html("<input type='radio' name='answer1' value='0'>" + "<label>" + questions[0].answerList[0] + "</label>"
+        + "<input type='radio' name='answer1' value='1'>" + "<label>" + questions[0].answerList[1] + "</label>"
+        + "<input type='radio' name='answer1' value='2'>" + "<label>" + questions[0].answerList[2] + "</label>"
+        + "<input type='radio' name='answer1' value='3'>" + "<label>" + questions[0].answerList[3] + "</label><br><br>"
+    );
 
-    $('#remaining-time').show();
-    
-    
-    trivia.nextQuestion();
-  }
-  nextQuestion : function(){
-    
-    
-    trivia.timer = 10;
-     $('#timer').removeClass('last-seconds');
-    $('#timer').text(trivia.timer);
-    
-    
-    if(!trivia.timerOn){
-      trivia.timerId = setInterval(trivia.timerRunning, 1000);
-    }
-    
-    
-    var questionContent = Object.values(trivia.questions)[trivia.currentSet];
-    $('#question').text(questionContent);
-    
-    // an array of all the user options for the current question
-    var questionOptions = Object.values(trivia.options)[trivia.currentSet];
-    
-    // creates all the trivia guess options in the html
-    $.each(questionOptions, function(index, key){
-      $('#options').append($('<button class="option btn btn-info btn-lg">'+key+'</button>'));
-    })
-    timerRunning : function(){
+    // Question 2
+    $("#question2").html("<h3>" + questions[1].question + "</h3>");
+    $("#answer2").html("<input type='radio' name='answer2' value='0'>" + "<label>" + questions[1].answerList[0] + "</label>"
+        + "<input type='radio' name='answer2' value='1'>" + "<label>" + questions[1].answerList[1] + "</label>"
+        + "<input type='radio' name='answer2' value='2'>" + "<label>" + questions[1].answerList[2] + "</label>"
+        + "<input type='radio' name='answer2' value='3'>" + "<label>" + questions[1].answerList[3] + "</label><br><br>"
+    );
 
-        if(trivia.timer > -1 && trivia.currentSet < Object.keys(trivia.questions).length){
-            $('#timer').text(trivia.timer);
-            trivia.timer--;
-              if(trivia.timer === 4){
-                $('#timer').addClass('last-seconds');
-              }
-          }
+    // Question 3
+    $("#question3").html("<h3>" + questions[2].question + "</h3>");
+    $("#answer3").html("<input type='radio' name='answer3' value='0'>" + "<label>" + questions[2].answerList[0] + "</label>"
+        + "<input type='radio' name='answer3' value='1'>" + "<label>" + questions[2].answerList[1] + "</label>"
+        + "<input type='radio' name='answer3' value='2'>" + "<label>" + questions[2].answerList[2] + "</label>"
+        + "<input type='radio' name='answer3' value='3'>" + "<label>" + questions[2].answerList[3] + "</label><br><br>"
+    );
+
+    // Question 4
+    $("#question4").html("<h3>" + questions[3].question + "</h3>");
+    $("#answer4").html("<input type='radio' name='answer4' value='0'>" + "<label>" + questions[3].answerList[0] + "</label>"
+        + "<input type='radio' name='answer4' value='1'>" + "<label>" + questions[3].answerList[1] + "</label>"
+        + "<input type='radio' name='answer4' value='2'>" + "<label>" + questions[3].answerList[2] + "</label>"
+        + "<input type='radio' name='answer4' value='3'>" + "<label>" + questions[3].answerList[3] + "</label><br><br>"
+    );
+
+    // Question 5
+    $("#question5").html("<h3>" + questions[4].question + "</h3>");
+    $("#answer5").html("<input type='radio' name='answer5' value='0'>" + "<label>" + questions[4].answerList[0] + "</label>"
+        + "<input type='radio' name='answer5' value='1'>" + "<label>" + questions[4].answerList[1] + "</label>"
+        + "<input type='radio' name='answer5' value='2'>" + "<label>" + questions[4].answerList[2] + "</label>"
+        + "<input type='radio' name='answer5' value='3'>" + "<label>" + questions[4].answerList[3] + "</label><br><br>"
+    );
+    // Question 6
+    $("#question6").html("<h3>" + questions[5].question + "</h3>");
+    $("#answer6").html("<input type='radio' name='answer6' value='0'>" + "<label>" + questions[5].answerList[0] + "</label>"
+        + "<input type='radio' name='answer6' value='1'>" + "<label>" + questions[5].answerList[1] + "</label>"
+        + "<input type='radio' name='answer6' value='2'>" + "<label>" + questions[5].answerList[2] + "</label>"
+        + "<input type='radio' name='answer6' value='3'>" + "<label>" + questions[5].answerList[3] + "</label><br><br>"
+    );
+    // Question 7
+    $("#question7").html("<h3>" + questions[6].question + "</h3>");
+    $("#answer7").html("<input type='radio' name='answer7' value='0'>" + "<label>" + questions[6].answerList[0] + "</label>"
+        + "<input type='radio' name='answer7' value='1'>" + "<label>" + questions[6].answerList[1] + "</label>"
+        + "<input type='radio' name='answer7' value='2'>" + "<label>" + questions[6].answerList[2] + "</label>"
+        + "<input type='radio' name='answer7' value='3'>" + "<label>" + questions[6].answerList[3] + "</label><br><br>"
+    );
+    // Question 8
+    $("#question8").html("<h3>" + questions[7].question + "</h3>");
+    $("#answer8").html("<input type='radio' name='answer7' value='0'>" + "<label>" + questions[7].answerList[0] + "</label>"
+        + "<input type='radio' name='answer7' value='1'>" + "<label>" + questions[7].answerList[1] + "</label>"
+        + "<input type='radio' name='answer7' value='2'>" + "<label>" + questions[7].answerList[2] + "</label>"
+        + "<input type='radio' name='answer7' value='3'>" + "<label>" + questions[7].answerList[3] + "</label><br><br>"
+    );
+
+
+    // Submit button
+    $("#submit").html("<button id='done' class='btn'>Done</button>");
+    // Click event 
+    $("#done").on("click", function() {
+    // Keeping track of score 
+        keepingScore();
+    // Display 
+        displayResults();
         
-          else if(trivia.timer === -1){
-            trivia.unanswered++;
-            trivia.result = false;
-            clearInterval(trivia.timerId);
-            resultId = setTimeout(trivia.guessResult, 1000);
-            $('#results').html('<h3>Out of time! The answer was '+ Object.values(trivia.answers)[trivia.currentSet] +'</h3>');
-          }
-          // if all the questions have been shown end the game, show results
-          else if(trivia.currentSet === Object.keys(trivia.questions).length){
-            
-            // adds results of game (correct, incorrect, unanswered) to the page
-            $('#results')
-              .html('<h3>Thank you for playing!</h3>'+
-              '<p>Correct: '+ trivia.correct +'</p>'+
-              '<p>Incorrect: '+ trivia.incorrect +'</p>'+
-              '<p>Unaswered: '+ trivia.unanswered +'</p>'+
-              '<p>Please play again!</p>');
-            
-            // hide game sction
-            $('#game').hide();
-            
-            // show start button to begin a new game
-            $('#start').show();
-          }
-          
-        },
-        // method to evaluate the option clicked
-        guessChecker : function() {
-          
-          // timer ID for gameResult setTimeout
-          var resultId;
-          
-          // the answer to the current question being asked
-          var currentAnswer = Object.values(trivia.answers)[trivia.currentSet];
-          
-          // if the text of the option picked matches the answer of the current question, increment correct
-          if($(this).text() === currentAnswer){
-            // turn button green for correct
-            $(this).addClass('btn-success').removeClass('btn-info');
-            
-            trivia.correct++;
-            clearInterval(trivia.timerId);
-            resultId = setTimeout(trivia.guessResult, 1000);
-            $('#results').html('<h3>Correct Answer!</h3>');
-          }
-          // else the user picked the wrong option, increment incorrect
-          else{
-            // turn button clicked red for incorrect
-            $(this).addClass('btn-danger').removeClass('btn-info');
-            
-            trivia.incorrect++;
-            clearInterval(trivia.timerId);
-            resultId = setTimeout(trivia.guessResult, 1000);
-            $('#results').html('<h3>Better luck next time! '+ currentAnswer +'</h3>');
-          }
-          
-        },
-        // method to remove previous question results and options
-        guessResult : function(){
-          
-          // increment to next question set
-          trivia.currentSet++;
-          
-          // remove the options and results
-          $('.option').remove();
-          $('#results h3').remove();
-          
-          // begin next question
-          trivia.nextQuestion();
-           
-        }
-      
-      }
+    });
+});
 
+// Timer countdown function
+function run() {clearInterval(intervalId);
+    intervalId = setInterval(decrement, 1000);
+}
+function decrement() {
+    //  Decrease number by one.
+    number--;
+    //  Show the number in the #time tag
+    $("#time").html("<h2>Time Remaining: " + number + " Seconds</h2>" + "<br>");
     
+    if (number === 0) {
+    // Run stop function to stop timer countdown
+     stop();
+     keepingScore();
+     displayResults();
+    }}
+ 
+    //  Clears intervalId
+function stop() {
+    //  Clears intervalId
+    clearInterval(intervalId);
+}
+//function for questions and answers
+function displayResults() {
+
+    $("#time").hide();
+    $("#question1").hide();
+    $("#answer1").hide();
+    $("#question2").hide();
+    $("#answer2").hide();
+    $("#question3").hide();
+    $("#answer3").hide();
+    $("#question4").hide();
+    $("#answer4").hide();
+    $("#question5").hide();
+    $("#answer5").hide();
+    $("#question6").hide();
+    $("#answer6").hide();
+    $("#question7").hide();
+    $("#answer7").hide();
+    $("#submit").hide();
+
+    $("#resultsMessage").html("<h3>All Done!</h3>");
+    $("#correct").html("Correct Answers: " + correctAnswers);
+    $("#incorrect").html("Incorrect Answers: " + incorrectAnswers);
+    $("#unanswered").html("Unanswered: " + unanswered);
+}
+
+
+// Function keeps score in terms of correct, incorrect, and unanswered 
+function keepingScore() {
+
+    let userAnswer1 = $("input[name='answer1']:checked").val();
+    let userAnswer2 = $("input[name='answer2']:checked").val();
+    let userAnswer3 = $("input[name='answer3']:checked").val();
+    let userAnswer4 = $("input[name='answer4']:checked").val();
+    let userAnswer5 = $("input[name='answer5']:checked").val();
+    let userAnswer6 = $("input[name='answer6']:checked").val();
+    let userAnswer7 = $("input[name='answer7']:checked").val();
+    let userAnswer8 = $("input[name='answer8']:checked").val();
+    // Question 1
+    if (userAnswer1 === undefined) {unanswered++;}
+        else if (userAnswer1 == questions[0].answer) {
+    correctAnswers++;}
+    else {incorrectAnswers++;}
+
+    // Question 2
+    if (userAnswer2 === undefined) {unanswered++;}
+        else if (userAnswer2 == questions[1].answer) {
+    correctAnswers++;}
+    else {incorrectAnswers++;
+    }
+
+    // Question 3
+    if (userAnswer3 === undefined) {unanswered++;}
+        else if (userAnswer3 == questions[2].answer) {
+    correctAnswers++;}
+    else {incorrectAnswers++;}
+
+   // Question 4
+    if (userAnswer4 === undefined) {unanswered++;}
+        else if (userAnswer4 == questions[3].answer) {
+    correctAnswers++;}
+    else {incorrectAnswers++;}
+    
+    
+    // Question 5
+    if (userAnswer5 === undefined) {unanswered++;}
+        else if (userAnswer3 == questions[4].answer) {
+    correctAnswers++;}
+    else {incorrectAnswers++;}
+    
+    
+    // Question 6
+    if (userAnswer6 === undefined) {unanswered++;}
+        else if (userAnswer6 == questions[5].answer) {
+    correctAnswers++;}
+    else {incorrectAnswers++;}
+    
+    // Question 7
+    if (userAnswer7 === undefined) { unanswered++;}
+    else if (userAnswer7 == questions[6].answer) {
+    correctAnswers++;}
+    else {incorrectAnswers++;
+    }  
+    // Question 8
+    if (userAnswer8 === undefined) { unanswered++;}
+    else if (userAnswer8 == questions[7].answer) {
+    correctAnswers++;}
+    else {incorrectAnswers++;
+    }  
+}
